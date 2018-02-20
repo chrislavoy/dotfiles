3c3802a8927b79fb0001edebbdd97684badf7c70
Chris LaVoy <chrislavoy@gmail.com>
2018-02-20 00:01:18 -0700

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Deploy Alacritty
+cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+echo "Deployed alacritty"
+
+# Deploy NeoVim or Vim
+if command -v nvim >/dev/null; then
+	cp vim/.vimrc ~/.config/nvim/init.vim
+	echo "Deployed nvim"
+else
+	cp vim/.vimrc ~/.vimrc
+	echo "Deployed vim"
+fi
+
+# Deploy ZSH or Bash
+if command -v zsh >/dev/null; then
+	cp zsh/.zshrc ~/.zshrc
+	echo "Deployed zsh"
+else
+	cp bash/.bash_profile ~/.bash_profile
+	echo "Deployed bash"
+fi
